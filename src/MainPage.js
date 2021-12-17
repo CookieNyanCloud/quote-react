d41e3af324669745a45e293c21ac89e5395b6b5b
CookieNyanCloud <emil8yunusov@gmail.com>
2021-12-17 18:12:31 +0300

    test

---
+++ src/MainPage.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import './MainPage.css'
+const MainPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const QuoteText = searchParams.get('quote-text')
+  const Name = searchParams.get('name')
+  const PhotoHeight = searchParams.get('photo-height')
+  const PhotoWidth = searchParams.get('photo-width')
+  const PhotoURL = searchParams.get('photo-url')
+
+  console.log(QuoteText, Name, PhotoHeight, PhotoWidth, PhotoURL)
+
+  const container = {
+    height: `${PhotoHeight}px`,
+    width: `${PhotoWidth}px`,
+    backgroundImage: `url(${PhotoURL})`,
+  }
+  const NameW = 30
+  const sotafontSize = PhotoWidth / NameW
+  const sotaletterSpacing = PhotoWidth / (NameW * 4)
+  const sota = {
+    fontSize: `${sotafontSize}px`,
+    letterSpacing: `${sotaletterSpacing}px`,
+  }
+  const minifontSize = PhotoWidth / (NameW * 2)
+  const mini = {
+    fontSize: `${minifontSize})px`,
+  }
+
+  const quoteTextfontSize = PhotoWidth / NameW
+  const quoteText = {
+    fontSize: `${quoteTextfontSize}px`,
+  }
+
+  const namefontSize = PhotoWidth / NameW
+  const name = {
+    fontSize: `${namefontSize}px`,
+  }
+
+  return (
+    <div>
+      <div className='container' style={container}>
+        <div className='logo-all'>
+          <div className='logo'>
+            <div className='full'>
+              <span className='sota' style={sota}>
+                SOTA
+              </span>
+              <span className='mini' style={mini}>
+                t.me/sotavision
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className='quote-text' style={quoteText}>
+          <p>{QuoteText}</p>
+        </div>
+        <div className='name' style={name}>
+          {Name}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
