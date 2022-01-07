f2c1f85ba9c8719040f443e15308a9dca69542e7
CookieNyanCloud <emil8yunusov@gmail.com>
2022-01-07 23:29:22 +0300

    new style

--- src/MainPage.js
+++ src/MainPage.js
@@ -17,53 +17,30 @@ const MainPage = () => {
   }
   const NameW = 25
 
-  const sotafontSize = PhotoWidth / NameW
-  const sotaletterSpacing = PhotoWidth / (NameW * 5)
-  const sota = {
-    fontSize: `${sotafontSize}px`,
-    letterSpacing: `${sotaletterSpacing}px`,
-  }
-
-  const minifontSize = (PhotoWidth * 2) / (NameW * 4)
-  const mini = {
-    fontSize: `${minifontSize}px`,
-  }
-
   const quoteTextfontSize = PhotoWidth / NameW
   const quoteText = {
     fontSize: `${quoteTextfontSize}px`,
   }
 
-  const namefontSize = PhotoWidth / NameW
-  const name = {
-    fontSize: `${namefontSize}px`,
-  }
-
   return (
-    <div>
-      <div className='container' style={container}>
-        <div className='logo-all'>
-          <div className='logo'>
-            <div className='full'>
-              <span className='sota' style={sota}>
-                SOTA
-              </span>
-              <span className='mini' style={mini}>
-                t.me/sotavision
-              </span>
-            </div>
-          </div>
+    <div className='container' style={container}>
+      <div className='up'>
+        <div className='logo'>
+          <div className='sota'>SOTA</div>
+          <div className='mini'>t.me/sotavision</div>
         </div>
-        <div className='quote-text' style={quoteText}>
-          <p>{QuoteText}</p>
+      </div>
+      <div className='quote'>
+        <div className='l'></div>
+        <div className='r'></div>
+        <div className='text' style={quoteText}>
+          <p>«{QuoteText}»</p>
         </div>
-        <div className='name' style={name}>
+      </div>
+      <div className='down'>
+        <div className='name'>
           {Name}
-          {Author?.length > 0 && (
-            <div className='name' style={name}>
-              {Author}
-            </div>
-          )}
+          {Author?.length > 0 && <div className='author'>{Author}</div>}
         </div>
       </div>
     </div>
